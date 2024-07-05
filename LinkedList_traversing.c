cd64790a3473d2dc0108f84404fb1f1f50b57e62
KIRITI AAJAD <112309709+Kiritiaajd@users.noreply.github.com>
2024-07-05 18:18:04 +0530

    LinkedList_traversing.c
    added functions like add at position

--- LinkedList_traversing.c
+++ LinkedList_traversing.c
@@ -43,18 +43,59 @@ void addfirst(struct Node ** head, int data){
        newNode->next=*head;
        *head = newNode;
 }
+  void addNode(struct Node **head , int data , int pos){
+    struct Node *newNode = malloc(sizeof(struct Node));
+    newNode->data=data;
+    newNode->next=NULL;
+     struct Node *ptr = *head;
+         if (*head == NULL || pos ==0)
+         {
+           *head = newNode;
+           return;
+         }
+            if (pos < 0)
+            {
+                printf("Invalid position");
+            }
+            
+         for (int i = 0; i < pos-2 && ptr->next != NULL; i++)    
+         {
+            ptr=ptr->next;
+         }
+         
+         newNode->next=ptr->next;
+         ptr->next=newNode;
+        
+         
+  }
+int sumElement(struct Node *head){
+    int sum = 0;
+     struct Node *ptr = head;
+     while (ptr !=NULL)
+     {
+        sum = sum+ptr->data;
+        ptr=ptr->next;
+     }
+     
+    return sum;
+}
 int main()
 {
     struct Node *head=NULL;
-    CreateLL(&head, 10);
-    CreateLL(&head, 20);
+  CreateLL(&head, 20);
     CreateLL(&head, 30);
+    CreateLL(&head, 40);
 
-
+    addfirst(&head,10);
+    printf("Actual list : \n");
     Display(head);
     printf("\n");
     addfirst(&head , 01);
+    printf("\nList after addding first Element\n");
     Display(head);
-
+    printf("\nAdding node at pos 3\n");
+    addNode(&head , 11 , 3);
+    Display(head);
+     printf("\nSum of Elements of list = %d",sumElement(head));
     return 0;
 }    

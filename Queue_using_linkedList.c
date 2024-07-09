e36a0d1bfa9dd6acdd72b4b11b6be78916817f94
KIRITI AAJAD <112309709+Kiritiaajd@users.noreply.github.com>
2024-07-10 03:39:52 +0530

    123

--- Queue_using_linkedList.c
+++ Queue_using_linkedList.c
@@ -1,18 +1,49 @@
-#include<stdio.h>
+#include <stdio.h>
 struct Node
 {
     int data;
-    struct Node* next;
+    struct Node *next;
 };
 struct Queue
 {
-    int *front;
-    int *rear;
-    struct Node* head;
+    struct Node *front;
+    struct Node *rear;
 };
-  
+void Display(struct Queue *q){
+      struct Node *ptr = q->front;
+      while (ptr != NULL)
+      {
+        printf("%d ", ptr->data);
+      }
+      printf("\n");
+      
+}
+void Enqueue(struct Queue *q, int data)
+{
+    struct Node *newNode = malloc(malloc(sizeof(struct Node)));
+   newNode->data = data;
+   newNode->next =NULL;
+      if (q->rear ==NULL)
+      {
+          q->front = q->rear = newNode;
 
-#include<stdlib.h>
-int main(){
+      }else{
+            q->rear->next = newNode;
+            q->rear = newNode;
+      }
+           
+}
+
+#include <stdlib.h>
+int main()
+{
+
+    // Intializing queue to NULL
+    struct Queue *q;
+    q->front = NULL;
+    q->rear = NULL;
+    Enqueue(q  , 10);
+    Enqueue(q , 20);
+    Display(q);
     return 0;
 }

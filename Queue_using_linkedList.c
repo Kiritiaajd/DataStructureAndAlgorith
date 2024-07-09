ba64f72d6054f15c6c778584ca013c38546b1f96
KIRITI AAJAD <112309709+Kiritiaajd@users.noreply.github.com>
2024-07-10 03:39:38 +0530

    Enqueue

---
+++ Queue_using_linkedList.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+struct Queue
+{
+    int *front;
+    int *rear;
+    struct Node* head;
+};
+  
+
+#include<stdlib.h>
+int main(){
+    return 0;
+}

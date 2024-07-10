cb20a095d4d8f1605f5a354cc264cdfc3cde20e2
KIRITI AAJAD <112309709+Kiritiaajd@users.noreply.github.com>
2024-07-11 01:04:23 +0530

    Insertion sort

--- InsertioSort_Using_LinkedList.c
+++ InsertioSort_Using_LinkedList.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 struct Node
 {
     int data;
     struct Node *next;
 };
 
-    
 void AddLast(struct Node **head, int data)
 {
     struct Node *newNode = malloc(sizeof(struct Node));
@@ -26,6 +26,7 @@ void AddLast(struct Node **head, int data)
         ptr->next = newNode;
     }
 }
+
 void Display(struct Node *head)
 {
     struct Node *ptr = head;
@@ -37,6 +38,33 @@ void Display(struct Node *head)
     printf("\n");
 }
 
+void InsertionSort(struct Node **head)
+{
+    struct Node *sorted = NULL;
+    struct Node *current = *head;
+    while (current != NULL)
+    {
+        struct Node *next = current->next;
+        struct Node *ptr;
+        if (sorted == NULL || sorted->data >= current->data)
+        {
+            current->next = sorted;
+            sorted = current;
+        }
+        else
+        {
+            ptr = sorted;
+            while (ptr->next != NULL && ptr->next->data < current->data)
+            {
+                ptr = ptr->next;
+            }
+            current->next = ptr->next;
+            ptr->next = current;
+        }
+        current = next;
+    }
+    *head = sorted;
+}
 
 int main()
 {
@@ -46,6 +74,14 @@ int main()
     AddLast(&head, 50);
     AddLast(&head, 40);
     AddLast(&head, 20);
+
+    printf("Original Linked List: ");
     Display(head);
+
+    InsertionSort(&head);
+
+    printf("Sorted Linked List: ");
+    Display(head);
+
     return 0;
 }
